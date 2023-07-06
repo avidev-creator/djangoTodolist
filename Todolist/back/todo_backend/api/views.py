f4b41b7c4949d649eb16411024875c0b7470312c
Avinash Dubey <123487788+avidev-creator@users.noreply.github.com>
2023-07-06 11:19:33 +0530

    bug fix
    minor bug fix

--- Todolist/back/todo_backend/api/views.py
+++ Todolist/back/todo_backend/api/views.py
@@ -73,7 +73,7 @@ class TaskCreate(LoginRequiredMixin, CreateView):
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 
